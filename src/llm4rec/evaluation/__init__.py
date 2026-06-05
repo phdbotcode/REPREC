1d97669e4410be60d88241bbafd972f9e00e72ef
phdbotcode <recsyscode@gmail.com>
2026-06-05 15:32:18 -0400

    Create __init__.py

---
+++ src/llm4rec/evaluation/__init__.py
@@ -0,0 +1,3 @@
+from llm4rec.evaluation.rank_eval import evaluate_ranker
+from llm4rec.evaluation.compute import count_params, estimate_train_flops, build_compute_record
+from llm4rec.evaluation.report import save_metrics, append_run_summary, load_all_summaries
