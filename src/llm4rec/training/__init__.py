eb8b852f00e76709619fb2df31ce316cd511e8d1
phdbotcode <recsyscode@gmail.com>
2026-06-05 15:40:12 -0400

    Create __init__.py

---
+++ src/llm4rec/training/__init__.py
@@ -0,0 +1,8 @@
+from llm4rec.training.optim import (
+    build_optimizer,
+    build_scheduler,
+    set_requires_grad,
+    grad_clip,
+)
+from llm4rec.training.train_injector import InjectorTrainer
+from llm4rec.training.train_lora import LoRATrainer
